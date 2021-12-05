c923051c68941c6fa93a4817568d98763711aedd
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2021-12-05 16:35:36 +0200

    added sentiment conclusion

--- data-analysis.py
+++ data-analysis.py
@@ -5,27 +5,67 @@ from textblob import TextBlob as textblob
 import matplotlib.pyplot as plot
 from datetime import datetime
 
-#Twitter API authentication
-f = open('auth.json', 'r')
-authData = json.load(f)
-f.close()
+#Twitter API authenticator
+def getOAuthHandler():
+    
+    f = open('auth.json', 'r')
+    authData = json.load(f)
+    f.close()
 
-consumerKey         = authData[0]
-consumerSecret      = authData[1]
-accessToken         = authData[2]
-accessTokenSecret   = authData[3]
+    consumerKey         = authData[0]
+    consumerSecret      = authData[1]
+    accessToken         = authData[2]
+    accessTokenSecret   = authData[3]
 
-auth = tweepy.OAuthHandler(
-    consumerKey, 
-    consumerSecret
-)
-auth.set_access_token(
-    accessToken, 
-    accessTokenSecret
-)
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(
+        consumerKey, 
+        consumerSecret
+    )
+    auth.set_access_token(
+        accessToken, 
+        accessTokenSecret
+    )
+    
+    return auth
 
-#Tweets sentiment analysis
+#Data analysis plotting
+def getSentimentPlot():
+
+    #Data
+    keyword = entrySearchKeyword.get()
+    sampleSize = int(entrySampleSize.get())
+
+    polarities = analyseTweetPolarities(keyword, sampleSize)
+    averagePolarity = (sum(polarities))/(len(polarities)) * 100
+    
+    sentiment = str(concludeSentiment(averagePolarity))
+    
+    time = datetime.now().strftime("\nAt: %H:%M" + "\nOn: %m-%d-%y")
+    averagePolarity = "{0:.0f}%".format(averagePolarity)
+    
+    #Plot
+    axes = plot.gca()
+    axes.set_ylim([-1, 2])
+
+    plot.scatter(range(1, sampleSize + 1), polarities)
+    
+    plot.text(1.75, 
+              1.25, 
+              "Average sentiment: " + averagePolarity + sentiment + time, 
+              fontsize = 12, 
+              bbox = dict(facecolor = 'none', 
+                          edgecolor = 'black', 
+                          boxstyle  = 'square, pad = 1'
+                          )
+              )
+
+    plot.title("Sentiment of " + keyword + " on Twitter")
+    plot.xlabel("Sample size")
+    plot.ylabel("Sentiment")
+    
+    plot.show()
+
+#Tweets polarity analysis
 def analyseTweetPolarities(keyword, sampleSize):
     
     polarities = []
@@ -45,39 +85,28 @@ def analyseTweetPolarities(keyword, sampleSize):
     
     return polarities
 
-#Data analysis plotting
-def getSentimentPlot():
+#Conclude overall sentiment based on average tweet polarity
+def concludeSentiment(p):
     
-    keyword = entrySearchKeyword.get()
-    sampleSize = int(entrySampleSize.get())
-
-    polarities = analyseTweetPolarities(keyword, sampleSize)
-
-    axes = plot.gca()
-    axes.set_ylim([-1, 2])
-
-    plot.scatter(range(1, sampleSize + 1), polarities)
-
-    averagePolarity = (sum(polarities))/(len(polarities))
+    sentiment = " Unknown"
     
-    averagePolarity = "{0:.0f}%".format(averagePolarity * 100)
+    if  -100 <= p < -50:
+        sentiment = " Very Negative"
+    elif -50 <= p < -10:
+        sentiment = " Negative"
+    elif -10 <= p <= 10:
+        sentiment = " Neutral"
+    elif  10 <  p <= 50:
+        sentiment = " Positive"
+    elif  50 <  p <= 100:
+        sentiment = " Very Positive"
+    else:
+        sentiment = " Out of range"
     
-    time = datetime.now().strftime("At: %H:%M\n" + "On: %m-%d-%y")
+    return sentiment
 
-    plot.text(1.75, 
-              1.25, 
-              "Average sentiment: " + str(averagePolarity) + "\n" + time, 
-              fontsize = 12, 
-              bbox = dict(facecolor = 'none', 
-                          edgecolor = 'black', 
-                          boxstyle = 'square, pad = 1'
-                          )
-              )
-
-    plot.title("Sentiment of " + keyword + " on Twitter")
-    plot.xlabel("Sample size")
-    plot.ylabel("Sentiment")
-    plot.show()
+#API Setup
+api = tweepy.API(getOAuthHandler())
 
 #Tkinter GUI
 root = Tk()
@@ -92,8 +121,10 @@ submit = Button(root, text = "Submit", command = getSentimentPlot)
 
 labelSearchKeyword.pack()
 entrySearchKeyword.pack()
+
 labelSampleSize.pack()
 entrySampleSize.pack()
+
 submit.pack(side = BOTTOM)
 
 root.mainloop()
